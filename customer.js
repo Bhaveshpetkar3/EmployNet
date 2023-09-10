68097745a36996ce8305a3799bb2a3c4e07f8af1
Prasanna2510 <129285980+Prasanna2510@users.noreply.github.com>
2023-09-11 03:03:10 +0530

    Update customer.js

--- customer.js
+++ customer.js
@@ -1,24 +1,27 @@
 let con = document.querySelector(".five25");
 let con2 = document.getElementsByClassName("five27");
 let con3 = document.createElement("div");
-con3.innerHTML = con.innerHTML;
 const back = document.querySelector(".close-window");
-for (let i = 0; i < 5; i++) {
-  con.innerHTML += con3.innerHTML;
+if(con){
+  con3.innerHTML = con.innerHTML;
+  for (let i = 0; i < 5; i++) {
+    con.innerHTML += con3.innerHTML;
+  }
 }
-
 let n = document.createElement("div");
 let n1 = document.querySelector(".five26");
 let n2 = document.querySelector(".five24");
 let n3 = document.querySelector(".five25");
-n3.style.height = n1.offsetHeight + "px";
-n.innerHTML = n1.innerHTML;
-if (window.innerWidth <= 500) {
-  n2.prepend(n);
-  n2.style.backgroundColor = "white";
-  n2.style.padding = "1rem 0";
-  n2.setAttribute("id", "fifth1");
-  n3.style.height = "100%";
+if(n1 && n2){
+  n3.style.height = n1.offsetHeight + "px";
+  n.innerHTML = n1.innerHTML;
+  if (window.innerWidth <= 500) {
+    n2.prepend(n);
+    n2.style.backgroundColor = "white";
+    n2.style.padding = "1rem 0";
+    n2.setAttribute("id", "fifth1");
+    n3.style.height = "100%";
+  }
 }
 
 let m = document.querySelectorAll(".five34");
@@ -30,8 +33,8 @@ for (let i of m) {
     }
   });
 }
-const modal = document.querySelector(".five62");
-const overlay = document.querySelector(".overlay");
+let modal = document.querySelector(".five62");
+let overlay = document.querySelector(".overlay");
 let prof = document.querySelector(".five3");
 prof.addEventListener("click", function () {
   //prof2.style.display="block";
@@ -53,3 +56,27 @@ document.addEventListener("keydown", function (event) {
 });
 back.addEventListener("click", closethemodal);
 overlay.addEventListener("click", closethemodal);
+
+let obs = new IntersectionObserver((entries)=>{
+  entries.forEach((entry) => {
+    if(entry.isIntersecting){
+      entry.target.classList.add("five84");
+    } else{
+      entry.target.classList.remove("five84");
+    }
+  });
+});
+let ani1 = document.querySelectorAll(".five84");
+ani1.forEach((i)=>{obs.observe(i)});
+
+let obs2 = new IntersectionObserver((entries)=>{
+  entries.forEach((entry) => {
+    if(entry.isIntersecting){
+      entry.target.classList.add("five85");
+    } else{
+      entry.target.classList.remove("five85");
+    }
+  });
+});
+let ani2 = document.querySelectorAll(".five85");
+ani2.forEach((i)=>{obs2.observe(i)});

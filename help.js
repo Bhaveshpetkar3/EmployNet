40e55aef37c080d5c977617cc0517d9f19b23eca
Prasanna2510 <129285980+Prasanna2510@users.noreply.github.com>
2023-09-17 17:22:23 +0530

    Update help.js

--- help.js
+++ help.js
@@ -1,13 +1,18 @@
 let b = document.getElementsByClassName("second8");
+let c = document.querySelector(".second15");
 for(let i of b){
     i.addEventListener("click", function(){
         let j = i.children;
-        if(j[1].style.display=="none"){
-            j[1].style.display="block";
-            j[2].style.display="block";
+        c.innerHTML=j[1].innerHTML;
+        c.style.width=j[1].clientWidth+"px";
+        if(j[1].classList.contains("second11")){
+            j[1].classList.remove("second11");
+            j[1].classList.add("second16");
+            j[1].style.height=c.offsetHeight+"px";
         } else{
-            j[1].style.display="none";
-            j[2].style.display="none";
+            j[1].classList.remove("second16");
+            j[1].classList.add("second11");
+            j[1].style.height="0";
         }
     });
 }
